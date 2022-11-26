d18b9fb9f084abe93f121d5fc1dacd091cfb04ed
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-11-26 23:47:42 +0200

    Tests passing, time to save WETH

--- src/WethDepegPreventooor.sol
+++ src/WethDepegPreventooor.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import {ERC20} from "solmate/tokens/ERC20.sol";
-import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {Ownable} from "openzeppelin/access/Ownable.sol";
+import {IERC20} from "openzeppelin/interfaces/IERC20.sol";
 
 /**
  * @title WethDepegPreventooor
@@ -12,20 +11,24 @@ import {Ownable} from "openzeppelin/access/Ownable.sol";
  * @notice AHHHHH IM PREVENTING THE DEPEG OF WETH!!!
  */
 contract WethDepegPreventooor is Ownable {
-    using SafeTransferLib for ERC20;
-
     uint private constant SCALE = 1e18;
 
-    ERC20 public immutable weth;
+    IWETH public constant weth = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
     uint public exchangeRate; // ETH received per WETH deposited, as a fraction of 1e18
 
-    constructor(ERC20 _weth) {
-        weth = _weth;
-    }
+    constructor() {}
 
     function dumpYourWETHBeforeItsTooLate(uint amount) external {
-        weth.safeTransferFrom(msg.sender, address(this), amount);
+        weth.transferFrom(msg.sender, address(this), amount);
+
+        uint ethAmount = (amount * exchangeRate) / SCALE;
+
+        weth.withdraw(ethAmount);
+
+        (bool sent, ) = payable(msg.sender).call{value: ethAmount}("");
+
+        require(sent, "AHHHH FKN RUGGED AGAIN :(");
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -39,6 +42,14 @@ contract WethDepegPreventooor is Ownable {
     }
 
     function withdrawWeth(uint256 amount, address to) public onlyOwner {
-        weth.safeTransfer(to, amount);
+        weth.transfer(to, amount);
     }
+
+    fallback() external payable {}
+
+    receive() external payable {}
+}
+
+interface IWETH is IERC20 {
+    function withdraw(uint wad) external;
 }
--- test/WethDepegPreventooor.t.sol
+++ test/WethDepegPreventooor.t.sol
@@ -7,10 +7,15 @@ import {WethDepegPreventooor} from "../src/WethDepegPreventooor.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
 contract WethDepegPreventooorForkTest is Test {
-    address constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; //TODO check this
+    address constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    address OWNER = address(321);
+    address USER = address(123);
 
     uint256 ethFork;
-    uint256 ETH_FORK_BLOCK = 35486670;
+    uint256 ETH_FORK_BLOCK = 16056800;
+
+    uint exchangeRate = 0.75e18;
 
     ERC20 weth;
     WethDepegPreventooor preventooor;
@@ -21,7 +26,11 @@ contract WethDepegPreventooorForkTest is Test {
         vm.rollFork(ETH_FORK_BLOCK);
 
         weth = ERC20(WETH_ADDRESS);
-        preventooor = new WethDepegPreventooor(weth);
+
+        vm.startPrank(OWNER);
+        preventooor = new WethDepegPreventooor();
+        preventooor.setExchangeRate(exchangeRate);
+        vm.stopPrank();
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -34,5 +43,25 @@ contract WethDepegPreventooorForkTest is Test {
 
     function testForkAtExpectedBlock() public {
         assertEq(block.number, ETH_FORK_BLOCK);
+    }
+
+    function testDumpWeth() public {
+        uint amount = 100e18;
+        uint expectedEthAmount = (amount * exchangeRate) / 1e18;
+
+        deal(address(weth), USER, amount);
+
+        assertEq(weth.balanceOf(address(preventooor)), 0);
+        assertEq(weth.balanceOf(USER), amount);
+        assertEq(USER.balance, 0);
+
+        vm.startPrank(USER);
+        weth.approve(address(preventooor), amount);
+        preventooor.dumpYourWETHBeforeItsTooLate(amount);
+        vm.stopPrank();
+
+        assertEq(weth.balanceOf(address(preventooor)), amount - expectedEthAmount);
+        assertEq(weth.balanceOf(USER), 0);
+        assertEq(USER.balance, expectedEthAmount);
     }
 }
